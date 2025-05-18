87d86288ce278125367de1bbae7330b926f686e7
yuktasarode <yuktasarode.sarode@gmail.com>
2025-05-18 13:16:15 -0400

    removed vectore files.

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,7 +1,5 @@
-// src/app/api/upload/route.ts
 'use server'
 import { NextRequest, NextResponse } from 'next/server';
-// import { vectorStore } from '@/utils/vector-store';
 import { addToChroma } from '@/utils/chroma';
 
 export async function POST(req: NextRequest) {
@@ -9,28 +7,6 @@ export async function POST(req: NextRequest) {
   console.log("Content",content)
   const chunks = content.match(/[\s\S]{1,300}/g) || [];
   const count = await addToChroma(chunks);
-
-//   for (const chunk of chunks) {
-//     console.log(chunk)
-//     const res = await fetch('http://localhost:11434/api/embeddings', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({
-//           model: 'nomic-embed-text', // ✅ Correct model for embeddings
-//           prompt: chunk,
-//         }),
-//       });
-//     const { embedding } = await res.json();
-//     vectorStore.push({ id: crypto.randomUUID(), embedding, chunk });
-//   }
-
-//   console.log('[VECTOR STORE]', vectorStore.map(v => ({
-//     id: v.id,
-//     chunk: v.chunk.slice(0, 100), // limit preview
-//     embeddingLength: v.embedding.length
-//   })));
-
-
 
   return NextResponse.json({ success: true, count: chunks.length });
 }
